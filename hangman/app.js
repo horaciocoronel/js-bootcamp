91f9b5239ee408d6a95f1cda19e2a64e839e3b57
horaciocoronel <arielc@gmail.com>
2018-05-08 15:12:09 -0400

    Primitive Objects part 2

--- hangman/app.js
+++ hangman/app.js
@@ -1,11 +1,21 @@
-// product --> Object.prototype --> null
+// Primitive value: string, number, boolean, null. undefined
 
-const product = {
-  name: 'Influence'
-}
+// Object: myObject --> Object.prototype --> null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function : myFunc --> Function.prototype --> Object.prototype --> null
+// String: myString --> String.prototype --> Object.prototype --> null
+// Number: myNumber --> Number.prototype --> Object.prototype --> null
+// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+const team = new Array(['Luke', 'Madison'])
+console.log(team.hasOwnProperty('filter'));
 
-// hasOwnProperty
-// console.log(product.hasOwnProperty('name')) // return true
-// console.log(product.hasOwnProperty('price')) // return false
-console.log(product.hasOwnProperty('hasOwnProperty')) // return false
-console.log(product)
+
+const getScore = () => 1;
+console.log(getScore);
+
+
+const product = 'Computer'
+console.log(product.split(""))
+
+const otherProduct = new String('Phone')
+console.log(otherProduct)
