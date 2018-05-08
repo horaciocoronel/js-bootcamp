378fc5d91deffb3eee084d7aaf8e5b280dd7f1bc
horaciocoronel <arielc@gmail.com>
2018-05-08 17:26:25 -0400

    Creating subclasses from classes we create

--- oop/person.js
+++ oop/person.js
@@ -21,6 +21,26 @@ class Person {
   }
 }
 
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, likes) {
+    super(firstName, lastName, age, likes)
+    this.position = position
+  }
+  getBio() {
+    return `${this.lastName} is a ${this.position}`
+    // name is a position
+  }
+  getYearsLeft() {
+    return 65 - this.age
+  }
+}
+
+const employee1 = new Employee('Claire', 'Doe', 23, 'Manager', ['biking'])
+console.log(employee1)
+employee1.setName('Alexis Doe');
+console.log(employee1.getBio());
+console.log(employee1.getYearsLeft())
+
 const person1 = new Person('John', 'Doe', 23, ['Reading', 'Biking'])
 
 person1.getBio = function() {
@@ -37,3 +57,25 @@ Person.prototype.getBio = function() {
   return 'testing testing'
 }
 console.log(person2.getBio());
+
+class Student extends Person {
+  constructor(firstName, lastName, age, grade, likes) {
+    super(firstName, lastName, age, likes);
+    this.grade = grade;
+  }
+  updateGrade(change) {
+    this.grade += change
+  }
+  getBio() {
+    const status = this.grade >= 70 ? 'passing' : 'failing'
+    return `${this.firstName} is ${status}`
+  }
+
+}
+
+const student1 = new Student('Laura', 'Doe', 23, 88, ['biking', 'reading'])
+console.log('-------------------------------');
+console.log(student1.getBio())
+student1.updateGrade(-20)
+console.log(student1.getBio())
+// console.log(student1)
