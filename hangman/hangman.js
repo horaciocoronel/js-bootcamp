738ab80f404d013c388b0c8af64b577fa1e5ad83
horaciocoronel <arielc@gmail.com>
2018-05-08 16:41:37 -0400

    Finish challenge part 4

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,7 +1,30 @@
 const Hangman = function(word, remainingGuesses) {
-  this.word = word.toLowerCase().split("")
-  this.remainingGuesses = remainingGuesses
-  this.guessedLetters = []
+  this.word = word.toLowerCase().split("");
+  this.remainingGuesses = remainingGuesses;
+  this.guessedLetters = [];
+  this.status = 'playing';
+}
+
+Hangman.prototype.calculateStatus = function() {
+  const finished = this.word.every((letter) => this.guessedLetters.includes(letter));
+
+  if(this.remainingGuesses === 0) {
+    this.status = 'failed'
+  } else if (finished) {
+    this.status = 'finished'
+  } else {
+    this.status = 'playing'
+  }
+}
+
+Hangman.prototype.getStatusMessage = function () {
+  if (this.status === 'playing') {
+    return `Guesses left: ${this.remainingGuesses}`
+  } else if (this.status === 'failed') {
+    return `Nice try! The word has ${this.word.join('')}`
+  } else {
+    return 'Great work! You guessed the word'
+  }
 }
 
 Hangman.prototype.getPuzzle = function () {
@@ -19,11 +42,15 @@ Hangman.prototype.getPuzzle = function () {
 Hangman.prototype.makeGuess = function (guess) {
   guess = guess.toLowerCase();
   const isUnique = !this.guessedLetters.includes(guess);
-  const isBadGuess = !this.word.includes(guess)
+  const isBadGuess = !this.word.includes(guess);
+  if(this.status !== 'playing') {
+    return 
+  }
   if(isUnique) {
     this.guessedLetters.push(guess)
   }
   if (isUnique && isBadGuess) {
     this.remainingGuesses-- // similar to this.remainingGuesses -= 1
   }
+  this.calculateStatus();
 }
