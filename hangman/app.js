b102c71b29ec29d0a5e45e44d92f17f315a00201
horaciocoronel <arielc@gmail.com>
2018-05-08 17:46:39 -0400

    add getters and setters

--- hangman/app.js
+++ hangman/app.js
@@ -2,13 +2,13 @@ const puzzleEl = document.querySelector("#puzzle");
 const guessesEl = document.querySelector("#guesses")
 const game1 = new Hangman('Cat', 2); 
 
-puzzleEl.textContent = game1.getPuzzle();
-guessesEl.textContent = game1.getStatusMessage();
+puzzleEl.textContent = game1.puzzle;
+guessesEl.textContent = game1.statusMessage;
 
 window.addEventListener('keypress', function(e) {
   const guess = String.fromCharCode(e.charCode);
   game1.makeGuess(guess)
-  puzzleEl.textContent = game1.getPuzzle()
-  guessesEl.textContent = game1.getStatusMessage();
+  puzzleEl.textContent = game1.puzzle
+  guessesEl.textContent = game1.statusMessage;
   console.log(game1.status)
 })
