a47208017a2ee40b8decf640c3d0a2d7a360a668
horaciocoronel <arielc@gmail.com>
2018-05-08 15:58:51 -0400

    refactor hangman into app.js file

--- hangman/app.js
+++ hangman/app.js
@@ -1,21 +1,13 @@
-// Primitive value: string, number, boolean, null. undefined
+const puzzleEl = document.querySelector("#puzzle");
+const guessesEl = document.querySelector("#guesses")
+const game1 = new Hangman('Cat', 2); 
 
-// Object: myObject --> Object.prototype --> null
-// Array: myArray --> Array.prototype --> Object.prototype --> null
-// Function : myFunc --> Function.prototype --> Object.prototype --> null
-// String: myString --> String.prototype --> Object.prototype --> null
-// Number: myNumber --> Number.prototype --> Object.prototype --> null
-// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
-const team = new Array(['Luke', 'Madison'])
-console.log(team.hasOwnProperty('filter'));
+puzzleEl.textContent = game1.getPuzzle();
+guessesEl.textContent = game1.remainingGuesses;
 
-
-const getScore = () => 1;
-console.log(getScore);
-
-
-const product = 'Computer'
-console.log(product.split(""))
-
-const otherProduct = new String('Phone')
-console.log(otherProduct)
+window.addEventListener('keypress', function(e) {
+  const guess = String.fromCharCode(e.charCode);
+  game1.makeGuess(guess)
+  puzzleEl.textContent = game1.getPuzzle()
+  guessesEl.textContent = game1.remainingGuesses;
+})
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,7 +1,3 @@
-// 1. Create Constructor function for the hangman game
-// 2. Setup two atributes. one for the word itself, another for the number of guesses allowed
-// 3. Create two instances of it and print both to the console
-
 const Hangman = function(word, remainingGuesses) {
   this.word = word.toLowerCase().split("")
   this.remainingGuesses = remainingGuesses
@@ -31,21 +27,3 @@ Hangman.prototype.makeGuess = function (guess) {
     this.remainingGuesses-- // similar to this.remainingGuesses -= 1
   }
 }
-
-const game1 = new Hangman('Cat', 2);
-// Guess c, t, z
-console.log(game1.getPuzzle()) // c*t
-// print remaining guesses (should be 1 )
-console.log(game1.remainingGuesses)
-
-const game2 = new Hangman('Toronto', 4)
-// Guess t
-console.log(game2.getPuzzle()); // t****t*
-console.log(game2.remainingGuesses)
-
-window.addEventListener('keypress', function(e) {
-  const guess = String.fromCharCode(e.charCode);
-  game1.makeGuess(guess)
-  console.log(game1.getPuzzle()) // c*t
-  console.log(game1.remainingGuesses)
-})
