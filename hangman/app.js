1e73a56dd301b5f4d89d93557c14d940a06a0c02
horaciocoronel <arielc@gmail.com>
2018-05-08 23:48:23 -0400

    Add error handling

--- hangman/app.js
+++ hangman/app.js
@@ -15,9 +15,11 @@ window.addEventListener('keypress', function(e) {
 
 const request = new XMLHttpRequest();
 request.addEventListener('readystatechange', (e) => {
-  if(e.target.readyState === 4) {
+  if(e.target.readyState === 4 && e.target.status === 200) {
     const data = JSON.parse(e.target.responseText);
     console.log(data)
+  } else if (e.target.readyState === 4) {
+    console.log('And the error has taken place')
   }
 }
 )
