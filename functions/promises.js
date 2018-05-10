8f2f4d27c4010526a3760aaeb5db6ba2f9598593
horaciocoronel <arielc@gmail.com>
2018-05-10 00:41:07 -0400

    Change function to return a promise

--- functions/promises.js
+++ functions/promises.js
@@ -15,13 +15,15 @@ getDataCallback((err, data) => {
 })
 
 // Promise
-const myPromise = new Promise((resolve, reject) => {
+const getDataPromise = (data) => new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve('this is the promise data');
-    reject('this is my promise error')
+    resolve(`this is the promise data ${data}`);
+    // reject('this is my promise error')
   },2000)
 });
 
+
+const myPromise = getDataPromise(123)
 myPromise
   .then((data) => {
     console.log(data)
