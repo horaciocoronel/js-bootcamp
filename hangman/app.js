056936035b404a7e8fd13fd6a9a5e50397092546
horaciocoronel <arielc@gmail.com>
2018-05-10 01:42:36 -0400

    Refactor GetCountryCode to use Promises and Catch for error handling

--- hangman/app.js
+++ hangman/app.js
@@ -13,33 +13,9 @@ window.addEventListener('keypress', function(e) {
 })
 
 getPuzzle('2')
-  .then((puzzle) => {
-    console.log(puzzle)
-  })
-  .catch((err) => {
-    console.log(`Error: ${err}`)
-  }
-  )
+  .then((puzzle) => console.log(puzzle))
+  .catch((err) => console.log(`Error: ${err}`))
 
 getCountryDetails("CA")
-  .then((country) => {
-    console.log(country.name)
-  }, (err) => {
-    console.log(`Error: ${err}`)
-  }
-  )
-
-// fetch('http://puzzle.mead.io/puzzle', {})
-//   .then((response) => {
-//     if(response.status === 200) {
-//       return response.json()
-//     } else {
-//       throw new Error(`Unable to fetch the puzzle`)
-//     }
-//   }
-//   )
-//   .then((data) => {
-//     console.log(`data promise`, data.puzzle)
-//   }
-//   )
-//   .catch((err) => console.log(err))
+  .then((country) => console.log(country.name))
+  .catch((err) => console.log(`Error: ${err}`))
--- hangman/requests.js
+++ hangman/requests.js
@@ -13,18 +13,14 @@ const getPuzzle = (wordCount) => {
     )
 }
 
-const getCountryDetails = (countryCode) => new Promise((resolve, reject) => {
-  const req = new XMLHttpRequest();
-  req.addEventListener('readystatechange', (e) => {
-    if(e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText); 
-    const countryData = data.find((country) => country.alpha2Code === countryCode)
-    resolve(countryData)
-    } else if (e.target.readyState === 4) {
-      reject('Something went wrong')
-    }
-  })
-  req.open('GET', 'https://restcountries.eu/rest/v2/all')
-  req.send()
-}
-)
+const getCountryDetails = (countryCode) => {
+  return fetch('https://restcountries.eu/rest/v2/all')
+    .then((response) => {
+      if(response.status === 200) {
+        return response.json()
+      } else {
+        throw new Error('Unable to fetch data')
+      }
+    })
+    .then((data) => data.find((country) => country.alpha2Code === countryCode))
+  }
