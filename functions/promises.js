ee0b30463a43f8487b3895be253022d918a7b23b
horaciocoronel <arielc@gmail.com>
2018-05-10 01:03:48 -0400

    explore promise chaining

--- functions/promises.js
+++ functions/promises.js
@@ -1,33 +1,53 @@
 // Callback
-const getDataCallback = (callback) => {
+const getDataCallback = (num, callback) => {
   setTimeout(() => {
-    // callback(undefined, 'this is the callback')
-    callback('this is the callback err')
+    if(typeof num === 'number') {
+      callback(undefined, num * 2)
+    } else {
+      callback('Number must be provided')
+    }
   }, 2000)
 }
 
-getDataCallback((err, data) => {
+getDataCallback(2,(err, data) => {
   if(err) {
     console.log(err)
   } else {
-    console.log(data)
+    getDataCallback(data, (err, data) => {
+      if(err) {
+        console.log(err)
+      } else {
+        console.log(`callback hell ${data}`)
+      }
+    }
+    )
   }
 })
 
 // Promise
-const getDataPromise = (data) => new Promise((resolve, reject) => {
+const getDataPromise = (num) => new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve(`this is the promise data ${data}`);
-    // reject('this is my promise error')
+    typeof num === 'number' ? resolve(num * 2) : reject('Number must be provided')
   },2000)
 });
 
 
-const myPromise = getDataPromise(123)
-myPromise
-  .then((data) => {
-    console.log(data)
-  }), (err) => {
+getDataPromise(2).then((data) => {
+    getDataPromise(data).then((data) => {
+        console.log(`promise deeply nested ${data}`)
+      }, (err) => {
+        console.log(err);
+      }
+      )
+  }, (err) => {
     console.log(err)
   }
+  )
+
   
+// Promise chaining
+  getDataPromise('10')
+    .then((data) => { return getDataPromise(data) })
+    .then((data) => { return getDataPromise(data) })
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
